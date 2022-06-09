6c999ce5223f6e8075bacb0be03e70f8bea7c31f
darknesshaotran <darknessclmm@gmail.com>
2022-06-09 16:33:10 +0700

    trần  hào

--- PBL3_Guna/Form/AddCategory.Designer.cs
+++ PBL3_Guna/Form/AddCategory.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.txtUserNameStaff = new Guna.UI2.WinForms.Guna2TextBox();
+            this.txtCategory = new Guna.UI2.WinForms.Guna2TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.guna2Elipse1 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
             this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
@@ -38,40 +38,41 @@
             this.guna2AnimateWindow1 = new Guna.UI2.WinForms.Guna2AnimateWindow(this.components);
             this.SuspendLayout();
             // 
-            // txtUserNameStaff
+            // txtCategory
             // 
-            this.txtUserNameStaff.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.txtUserNameStaff.BorderRadius = 6;
-            this.txtUserNameStaff.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.txtUserNameStaff.DefaultText = "";
-            this.txtUserNameStaff.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
-            this.txtUserNameStaff.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
-            this.txtUserNameStaff.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
-            this.txtUserNameStaff.DisabledState.Parent = this.txtUserNameStaff;
-            this.txtUserNameStaff.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
-            this.txtUserNameStaff.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.txtUserNameStaff.FocusedState.Parent = this.txtUserNameStaff;
-            this.txtUserNameStaff.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtUserNameStaff.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.txtUserNameStaff.HoverState.Parent = this.txtUserNameStaff;
-            this.txtUserNameStaff.Location = new System.Drawing.Point(119, 39);
-            this.txtUserNameStaff.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.txtUserNameStaff.Name = "txtUserNameStaff";
-            this.txtUserNameStaff.PasswordChar = '\0';
-            this.txtUserNameStaff.PlaceholderText = "";
-            this.txtUserNameStaff.SelectedText = "";
-            this.txtUserNameStaff.ShadowDecoration.Parent = this.txtUserNameStaff;
-            this.txtUserNameStaff.Size = new System.Drawing.Size(211, 24);
-            this.txtUserNameStaff.TabIndex = 32;
+            this.txtCategory.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtCategory.BorderRadius = 6;
+            this.txtCategory.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtCategory.DefaultText = "";
+            this.txtCategory.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtCategory.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtCategory.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtCategory.DisabledState.Parent = this.txtCategory;
+            this.txtCategory.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtCategory.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtCategory.FocusedState.Parent = this.txtCategory;
+            this.txtCategory.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCategory.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtCategory.HoverState.Parent = this.txtCategory;
+            this.txtCategory.Location = new System.Drawing.Point(159, 48);
+            this.txtCategory.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.txtCategory.Name = "txtCategory";
+            this.txtCategory.PasswordChar = '\0';
+            this.txtCategory.PlaceholderText = "";
+            this.txtCategory.SelectedText = "";
+            this.txtCategory.ShadowDecoration.Parent = this.txtCategory;
+            this.txtCategory.Size = new System.Drawing.Size(281, 30);
+            this.txtCategory.TabIndex = 32;
             // 
             // label2
             // 
             this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(28, 45);
+            this.label2.Location = new System.Drawing.Point(37, 55);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(57, 18);
+            this.label2.Size = new System.Drawing.Size(72, 23);
             this.label2.TabIndex = 31;
             this.label2.Text = "Name:";
             // 
@@ -95,10 +96,11 @@
             this.guna2Button1.HoverState.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.guna2Button1.HoverState.ForeColor = System.Drawing.Color.White;
             this.guna2Button1.HoverState.Parent = this.guna2Button1;
-            this.guna2Button1.Location = new System.Drawing.Point(206, 106);
+            this.guna2Button1.Location = new System.Drawing.Point(275, 130);
+            this.guna2Button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.guna2Button1.Name = "guna2Button1";
             this.guna2Button1.ShadowDecoration.Parent = this.guna2Button1;
-            this.guna2Button1.Size = new System.Drawing.Size(109, 45);
+            this.guna2Button1.Size = new System.Drawing.Size(145, 55);
             this.guna2Button1.TabIndex = 46;
             this.guna2Button1.Text = "Exit";
             // 
@@ -117,12 +119,14 @@
             this.btnLogin.HoverState.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnLogin.HoverState.ForeColor = System.Drawing.Color.White;
             this.btnLogin.HoverState.Parent = this.btnLogin;
-            this.btnLogin.Location = new System.Drawing.Point(31, 106);
+            this.btnLogin.Location = new System.Drawing.Point(41, 130);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnLogin.Name = "btnLogin";
             this.btnLogin.ShadowDecoration.Parent = this.btnLogin;
-            this.btnLogin.Size = new System.Drawing.Size(109, 45);
+            this.btnLogin.Size = new System.Drawing.Size(145, 55);
             this.btnLogin.TabIndex = 45;
             this.btnLogin.Text = "Add";
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
             // 
             // guna2DragControl1
             // 
@@ -131,15 +135,16 @@
             // 
             // AddCategory
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(216)))), ((int)(((byte)(176)))));
-            this.ClientSize = new System.Drawing.Size(343, 173);
+            this.ClientSize = new System.Drawing.Size(457, 213);
             this.Controls.Add(this.guna2Button1);
             this.Controls.Add(this.btnLogin);
-            this.Controls.Add(this.txtUserNameStaff);
+            this.Controls.Add(this.txtCategory);
             this.Controls.Add(this.label2);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "AddCategory";
             this.Text = "AddCategory";
             this.ResumeLayout(false);
@@ -149,7 +154,7 @@
 
         #endregion
 
-        private Guna.UI2.WinForms.Guna2TextBox txtUserNameStaff;
+        private Guna.UI2.WinForms.Guna2TextBox txtCategory;
         private System.Windows.Forms.Label label2;
         private Guna.UI2.WinForms.Guna2Elipse guna2Elipse1;
         private Guna.UI2.WinForms.Guna2Button guna2Button1;
--- PBL3_Guna/Form/AddCategory.cs
+++ PBL3_Guna/Form/AddCategory.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BUS;
+using DTO;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +17,21 @@ namespace PBL3_Guna
         public AddCategory()
         {
             InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            CategoryDTO CT = new CategoryDTO();
+            CT.Name = txtCategory.Text;
+            if (txtCategory.Text.Trim() == "")
+            {
+                MessageBox.Show("Vui lòng nhập tên mặt hàng !");
+            }
+            else
+            {
+                CategoryBUS.Instance.AddCategory(CT);
+                this.Close();
+            }
         }
     }
 }
